53159c70a44fe3831a4d0f9267f47055d2662918
weskhiu <reimi1021@icloud.com>
2026-05-23 12:44:49 +0900

    add ABC014

---
+++ ABC014/A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+  char X;
+  scanf("%c", &X);
+  if (X == 'A'){
+    printf("1\n");
+  }
+  else if (X == 'B'){
+    printf("2\n");
+  }
+  else if (X == 'C'){
+    printf("3\n");
+  }
+  else if (X == 'D'){
+    printf("4\n");
+  }
+  else if (X == 'E'){
+    printf("5\n");
+  }
+  return 0;
+}
